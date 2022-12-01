a1a899a9a469f1a0ee7bcc7714244287a8170aa2
Jessé Martins <jessemartins.ti@gmail.com>
2022-12-01 13:51:26 -0300

    finalization of the button component

---
+++ src/components/buttons/Buttons.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Button.scss';
+
+export function ButtonSmallOrange(props) {
+    return (
+        <button className='dt-btn-small-orange hvr-bob'>{props.value}</button>
+    )
+}
+
+export function ButtonDefaultOrange(props) {
+    return (
+        <button className='dt-btn-default-orange hvr-bob'>{props.value}</button>
+    )
+}
+
+export function ButtonLargeOrange(props) {
+    return (
+        <button className='dt-btn-large-orange hvr-bob'>{props.value}</button>
+    )
+}
+
+export function ButtonSmallGray(props) {
+    return (
+        <button className='dt-btn-small-gray hvr-bob'>{props.value}</button>
+    )
+}
+
+export function ButtonDefaultGray(props) {
+    return (
+        <button className='dt-btn-default-gray hvr-bob'>{props.value}</button>
+    )
+}
+
+export function ButtonLargeGray(props) {
+    return (
+        <button className='dt-btn-large-gray hvr-bob'>{props.value}</button>
+    )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Home.scss';
 import Brand from '../../assets/icons/brand-white.svg';
-
+import { ButtonDefaultGray, ButtonDefaultOrange, ButtonLargeGray, ButtonLargeOrange, ButtonSmallGray, ButtonSmallOrange } from '../../components/buttons/Buttons';
+/**
+ * 
+ *  <main className="dt-home">
+      <img src={Brand} alt="Brand" />
+    </main> 
+ */
 export default function Home() {
   return (
     <React.Fragment>
-        <main className="dt-home">
-            <img src={Brand} alt="Brand" />
-        </main>
+      
     </React.Fragment>
   )
 }
