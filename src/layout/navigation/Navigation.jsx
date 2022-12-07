26df79a05ad20bd8a0d8f2ffc422dad91dd7d4c6
Jessé Martins <jessemartins.ti@gmail.com>
2022-12-07 13:17:16 -0300

    update links project

--- src/layout/navigation/Navigation.jsx
+++ src/layout/navigation/Navigation.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './Navigation.scss';
 import { ButtonSmallGray, ButtonSmallOrange } from '../../components/buttons/Buttons';
