b7ceeb66e77b8ca7da81ed57f6bf36935cddcd32
Jessé Martins <jessemartins.ti@gmail.com>
2022-12-07 13:14:22 -0300

    update links project

--- src/layout/navigation-mobile/NavigationMobile.jsx
+++ src/layout/navigation-mobile/NavigationMobile.jsx
@@ -25,7 +25,7 @@ export default function NavigationMobile() {
                             <a class="nav-link" href="/about">About</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="/#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Services
                             </a>
                             <ul class="dropdown-menu">
